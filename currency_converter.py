41bc4d2cedaddcc22ab1a38bc8fad71b11648bd4
jhassler1031 <jhassler1031@gmail.com>
2018-05-23 14:42:42 -0400

    initial setup of conversion methods

--- currency_converter.py
+++ currency_converter.py
@@ -10,6 +10,55 @@ class Money:
 #Start of __init__ method
     def __init__(self, amount, currency):
         self.amount = amount
-        self.current = currency
+        self.currency = currency
 
-        
+
+#Start of __str__ method
+    def __str__(self):
+        return str(self.amount) + " " + self.currency
+
+#Start of conversion to Japanese Yen (JPY)
+
+    def conv_to_jpy(self):
+
+        rate = 111.2
+        if self.currency != "JPY":
+            return self.amount * rate
+        else:
+            return self.amount
+
+#Start of conversion to Euro (EUR)
+
+    def conv_to_eur(self):
+
+        rate = 0.85
+        if self.currency != "EUR":
+            return self.amount * rate
+        else:
+            return self.amount
+
+#Start of conversion to Bitcoin (XBT)
+
+    def conv_to_xbt(self):
+
+        rate = 0.00013
+        if self.currency != "XBT":
+            return str(self.amount * rate) + "XBT"
+        else:
+            return self.amount
+
+#Start of coversion to US Dollars (USD)
+
+    def conv_to_usd(self):
+
+        pass
+
+
+
+#Start of main
+
+x = Money(50, "XBT")
+
+print(x)
+
+print(x.conv_to_xbt())
