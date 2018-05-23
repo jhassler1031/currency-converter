1d4b4ce8496534326698a6ce4a49bcec93130fa1
jhassler1031 <jhassler1031@gmail.com>
2018-05-23 15:25:27 -0400

    normal mode first iteration

--- currency_converter.py
+++ currency_converter.py
@@ -11,54 +11,83 @@ class Money:
     def __init__(self, amount, currency):
         self.amount = amount
         self.currency = currency
+        self.jpy_rate = 110.2
+        self.eur_rate = 0.85
+        self.xbt_rate = 0.00013
 
 
 #Start of __str__ method
     def __str__(self):
         return str(self.amount) + " " + self.currency
 
+#Start of convert method
+
+    def convert(self, to_currency):
+        conv_amount = 0
+        conv_currency = ""
+
+        if to_currency == "JPY":
+            conv_amount = self.amount * self.jpy_rate
+            conv_currency = "JPY"
+        elif to_currency == "EUR":
+            conv_amount = self.amount * self.eur_rate
+            conv_currency = "EUR"
+        elif to_currency == "XBT":
+            conv_amount = self.amount * self.xbt_rate
+            conv_currency = "XBT"
+        else:
+            if self.currency == "JPY":
+                conv_amount = self.amount / self.jpy_rate
+            elif self.currency == "EUR":
+                conv_amount = self.amount / self.eur_rate
+            elif self.currency == "XBT":
+                conv_amount = self.amount / self.xbt_rate
+            else:
+                conv_amount = self.amount
+            conv_currency = "USD"
+
+        return Money(conv_amount, conv_currency)
+
 #Start of conversion to Japanese Yen (JPY)
 
     def conv_to_jpy(self):
-
-        rate = 111.2
         if self.currency != "JPY":
-            return self.amount * rate
+            return self.convert("JPY")
         else:
-            return self.amount
+            return Money(self.amount, self.currency)
 
 #Start of conversion to Euro (EUR)
 
     def conv_to_eur(self):
-
-        rate = 0.85
         if self.currency != "EUR":
-            return self.amount * rate
+            return self.convert("EUR")
         else:
-            return self.amount
+            return Money(self.amount, self.currency)
 
 #Start of conversion to Bitcoin (XBT)
 
     def conv_to_xbt(self):
-
-        rate = 0.00013
         if self.currency != "XBT":
-            return str(self.amount * rate) + "XBT"
+            return self.convert("XBT")
         else:
-            return self.amount
+            return Money(self.amount, self.currency)
 
 #Start of coversion to US Dollars (USD)
 
     def conv_to_usd(self):
+        if self.currency != "USD":
+            return self.convert("USD")
+        else:
+            return Money(self.amount, self.currency)
 
-        pass
 
 
 
 #Start of main
 
-x = Money(50, "XBT")
+x = Money(50, "USD")
 
 print(x)
-
 print(x.conv_to_xbt())
+print(x.conv_to_eur())
+print(x.conv_to_jpy())
